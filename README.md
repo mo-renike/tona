91a5de8d8178b37f672c0820579803137ed056e3
Morenike Oyewole <65955563+mo-renike@users.noreply.github.com>
2021-12-01 11:52:42 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# tona
+# tona
+# tona
---
+++ javascripts/main.js
@@ -0,0 +1,27 @@
+// Sticky Navbar
+const navbar = document.querySelector("header");
+let sticky = navbar.offsetTop;
+const navbarScroll = () => {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+window.onscroll = navbarScroll;
+
+
+console.log(navbar);
+
+//navigation 
+
+const nav = document.querySelector('.nav')
+const toggler = document.querySelector('.toggler')
+const close = document.querySelector('.nav__close')
+
+toggler.addEventListener('click', () =>{
+nav.classList.add('show')
+})
+close.addEventListener('click', () =>{
+nav.classList.remove('show')
+})
